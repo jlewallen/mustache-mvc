cb998b9929459421c0954c917285ab591a2e56a4
Jacob Lewallen <jlewallen@solutionstream.com>
2011-10-15 10:58:23 -0700

    Cleaning up some redundancy.

--- src/main/java/com/page5of4/mustache/spring/MustacheView.java
+++ src/main/java/com/page5of4/mustache/spring/MustacheView.java
@@ -3,23 +3,20 @@ package com.page5of4.mustache.spring;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.ServletConfigAware;
 import org.springframework.web.servlet.view.AbstractUrlBasedView;
 
 import com.page5of4.mustache.MustacheViewEngine;
 
-public class MustacheView extends AbstractUrlBasedView implements ServletConfigAware {
-
+public class MustacheView extends AbstractUrlBasedView {
    private static Logger logger = LoggerFactory.getLogger(MustacheView.class);
+   private static final String DEFAULT_LAYOUT_NAME = "layout";
    private MustacheViewEngine engine;
-   private ServletConfig servletConfig;
 
    public MustacheViewEngine getEngine() {
       return engine;
@@ -40,32 +37,18 @@ public class MustacheView extends AbstractUrlBasedView implements ServletConfigA
 
    @Override
    public boolean checkResource(final Locale locale) throws Exception {
-      LayoutAndView lav = getLayoutAndView(getUrl());
+      LayoutAndView lav = LayoutAndView.getLayoutAndView(getUrl(), DEFAULT_LAYOUT_NAME);
       return engine.containsView(lav.getView());
    }
 
    @Override
    protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
       try {
-         LayoutAndView lav = LayoutAndView.getLayoutAndView(getUrl(), "layout");
+         LayoutAndView lav = LayoutAndView.getLayoutAndView(getUrl(), DEFAULT_LAYOUT_NAME);
          engine.render(lav, model, response.getWriter());
       }
       catch(Exception error) {
          throw new RuntimeException(String.format("Error rendering %s", getUrl()), error);
       }
    }
-
-   public LayoutAndView getLayoutAndView(String url) {
-      String[] parts = url.split(":");
-      if(parts.length == 1) {
-         return new LayoutAndView(null, parts[0]);
-      }
-      return new LayoutAndView(parts[0], parts[1]);
-   }
-
-   @Override
-   public void setServletConfig(ServletConfig servletConfig) {
-      this.servletConfig = servletConfig;
-   }
-
 }
