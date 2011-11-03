052de1f7bd9788d5da1811fcaff47627c92fe903
Jacob Lewallen <jlewallen@gmail.com>
2011-11-02 19:15:20 -0700

    Automatic generation of body model JSON... may want this to be lazy...

--- src/main/java/com/page5of4/mustache/DefaultLayoutViewModelFactory.java
+++ src/main/java/com/page5of4/mustache/DefaultLayoutViewModelFactory.java
@@ -4,11 +4,16 @@ import java.util.Map;
 
 import javax.servlet.ServletConfig;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.ServletConfigAware;
 
 public class DefaultLayoutViewModelFactory implements LayoutViewModelFactory, ServletConfigAware {
 
+   private static final Logger logger = LoggerFactory.getLogger(DefaultLayoutViewModelFactory.class);
    private ServletConfig servletConfig;
+   private final ObjectMapper objectMapper = new ObjectMapper();
 
    @Override
    public LayoutViewModel createLayoutViewModel(Map<String, Object> model) {
@@ -19,16 +24,31 @@ public class DefaultLayoutViewModelFactory implements LayoutViewModelFactory, Se
    public LayoutViewModel createLayoutViewModel(Map<String, Object> model, LayoutBodyFunction bodyFunction) {
       final ApplicationModel applicationModel = createApplicationModel();
       model.put(SingleModelAndView.APPLICATION_MODEL_NAME, applicationModel);
-      return new LayoutViewModel(applicationModel, SingleModelAndView.getBodyModel(model), SingleModelAndView.getLayoutModel(model), bodyFunction);
-   }
-
-   private ApplicationModel createApplicationModel() {
-      return new ApplicationModel(servletConfig.getServletContext().getContextPath());
+      String bodyModelAsJSON = getBodyModelAsJSON(SingleModelAndView.getBodyModel(model));
+      return new LayoutViewModel(applicationModel, SingleModelAndView.getBodyModel(model), bodyModelAsJSON, SingleModelAndView.getLayoutModel(model), bodyFunction);
    }
 
    @Override
    public void setServletConfig(ServletConfig servletConfig) {
       this.servletConfig = servletConfig;
+   }
+
+   protected String getBodyModelAsJSON(Object bodyModel) {
+      try {
+         return objectMapper.writeValueAsString(bodyModel);
+      }
+      catch(Exception e) {
+         logger.error("Error converting BodyModel to JSON", e);
+         return null;
+      }
+   }
+
+   protected ApplicationModel createApplicationModel() {
+      return new ApplicationModel(servletConfig.getServletContext().getContextPath());
+   }
+
+   protected String encodeURL(String url) {
+      return servletConfig.getServletContext().getContextPath() + "/" + url;
    }
 
 }
--- src/main/java/com/page5of4/mustache/LayoutViewModel.java
+++ src/main/java/com/page5of4/mustache/LayoutViewModel.java
@@ -1,11 +1,11 @@
 package com.page5of4.mustache;
 
 public class LayoutViewModel {
-
    private final ApplicationModel applicationModel;
    private final LayoutBodyFunction layoutBodyFunction;
    private final Object bodyModel;
    private final Object layoutModel;
+   private final String bodyModelAsJSON;
 
    public ApplicationModel getApplicationModel() {
       return applicationModel;
@@ -22,16 +22,20 @@ public class LayoutViewModel {
       return bodyModel;
    }
 
+   public String getBodyModelAsJSON() {
+      return bodyModelAsJSON;
+   }
+
    public Object getLayoutModel() {
       return layoutModel;
    }
 
-   public LayoutViewModel(ApplicationModel applicationModel, Object bodyModel, Object layoutModel, LayoutBodyFunction layoutBodyFunction) {
+   public LayoutViewModel(ApplicationModel applicationModel, Object bodyModel, String bodyModelAsJSON, Object layoutModel, LayoutBodyFunction layoutBodyFunction) {
       super();
       this.applicationModel = applicationModel;
       this.bodyModel = bodyModel;
+      this.bodyModelAsJSON = bodyModelAsJSON;
       this.layoutModel = layoutModel;
       this.layoutBodyFunction = layoutBodyFunction;
    }
-
 }
