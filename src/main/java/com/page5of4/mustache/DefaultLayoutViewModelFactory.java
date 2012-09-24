8359b9362f91e22a03e1b3b9017f80e8d4d83369
Jacob Lewallen <jlewallen@gmail.com>
2012-09-24 14:36:22 -0700

    Return DefaultLayoutViewModel from DefaultLaayoutViewModelFactory.

--- src/main/java/com/page5of4/mustache/DefaultLayoutViewModelFactory.java
+++ src/main/java/com/page5of4/mustache/DefaultLayoutViewModelFactory.java
@@ -13,54 +13,55 @@ import org.springframework.web.context.ServletConfigAware;
 import com.samskivert.mustache.Mustache;
 
 public class DefaultLayoutViewModelFactory implements LayoutViewModelFactory, ServletConfigAware {
-	private static final Logger logger = LoggerFactory.getLogger(DefaultLayoutViewModelFactory.class);
-	private ServletConfig servletConfig;
-	private final ObjectMapper objectMapper = new ObjectMapper();
+   private static final Logger logger = LoggerFactory.getLogger(DefaultLayoutViewModelFactory.class);
+   private ServletConfig servletConfig;
+   private final ObjectMapper objectMapper = new ObjectMapper();
 
-	@Override
-	public LayoutViewModel createLayoutViewModel(Map<String, Object> model) {
-		return createLayoutViewModel(model, null);
-	}
+   @Override
+   public LayoutViewModel createLayoutViewModel(Map<String, Object> model) {
+      return createLayoutViewModel(model, null);
+   }
 
-	@Autowired(required = false)
-	private I18nLambdaFactory localizationFactory;
+   @Autowired(required = false)
+   private I18nLambdaFactory localizationFactory;
 
-	@Override
-	public LayoutViewModel createLayoutViewModel(Map<String, Object> model, LayoutBodyFunction bodyFunction) {
-		return createLayoutViewModel(model, bodyFunction, null);
-	}
+   @Override
+   public DefaultLayoutViewModel createLayoutViewModel(Map<String, Object> model, LayoutBodyFunction bodyFunction) {
+      return createLayoutViewModel(model, bodyFunction, null);
+   }
 
-	@Override
-	public LayoutViewModel createLayoutViewModel(Map<String, Object> model, LayoutBodyFunction bodyFunction, LayoutHeadersFunction headersFunction) {
-		final ApplicationModel applicationModel = createApplicationModel();
-		model.put(SingleModelAndView.APPLICATION_MODEL_NAME, applicationModel);
-		String bodyModelAsJSON = getBodyModelAsJSON(SingleModelAndView.getBodyModel(model));
-		Mustache.Lambda i18nLambda = null;
-		if (localizationFactory != null) {
-			i18nLambda = localizationFactory.getI18nLambda();
-		}
-		return new DefaultLayoutViewModel(applicationModel, SingleModelAndView.getBodyModel(model), bodyModelAsJSON, SingleModelAndView.getLayoutModel(model), bodyFunction, headersFunction, i18nLambda);
-	}
+   @Override
+   public DefaultLayoutViewModel createLayoutViewModel(Map<String, Object> model, LayoutBodyFunction bodyFunction, LayoutHeadersFunction headersFunction) {
+      final ApplicationModel applicationModel = createApplicationModel();
+      model.put(SingleModelAndView.APPLICATION_MODEL_NAME, applicationModel);
+      String bodyModelAsJSON = getBodyModelAsJSON(SingleModelAndView.getBodyModel(model));
+      Mustache.Lambda i18nLambda = null;
+      if(localizationFactory != null) {
+         i18nLambda = localizationFactory.getI18nLambda();
+      }
+      return new DefaultLayoutViewModel(applicationModel, SingleModelAndView.getBodyModel(model), bodyModelAsJSON, SingleModelAndView.getLayoutModel(model), bodyFunction, headersFunction, i18nLambda);
+   }
 
-	@Override
-	public void setServletConfig(ServletConfig servletConfig) {
-		this.servletConfig = servletConfig;
-	}
+   @Override
+   public void setServletConfig(ServletConfig servletConfig) {
+      this.servletConfig = servletConfig;
+   }
 
-	protected String getBodyModelAsJSON(Object bodyModel) {
-		try {
-			return objectMapper.writeValueAsString(bodyModel);
-		} catch (Exception e) {
-			logger.error("Error converting BodyModel to JSON", e);
-			return null;
-		}
-	}
+   protected String getBodyModelAsJSON(Object bodyModel) {
+      try {
+         return objectMapper.writeValueAsString(bodyModel);
+      }
+      catch(Exception e) {
+         logger.error("Error converting BodyModel to JSON", e);
+         return null;
+      }
+   }
 
-	protected ApplicationModel createApplicationModel() {
-		return new ApplicationModel(servletConfig.getServletContext().getContextPath());
-	}
+   protected ApplicationModel createApplicationModel() {
+      return new ApplicationModel(servletConfig.getServletContext().getContextPath());
+   }
 
-	protected String encodeURL(String url) {
-		return servletConfig.getServletContext().getContextPath() + "/" + url;
-	}
+   protected String encodeURL(String url) {
+      return servletConfig.getServletContext().getContextPath() + "/" + url;
+   }
 }
